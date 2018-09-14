375b76717a1a90d60a489665df042fe6bc999d30
V. R <for_usenet@yahoo.com>
2018-09-14 10:06:10 -0400

    Most of machine customization machinery is in the script added here.

---
+++ createMachine.sh
@@ -0,0 +1,55 @@
+
+#!/bin/sh
+
+# Useful external repositories for SuSE.
+zypper ar -f http://download.opensuse.org/update/42.3/ update
+zypper ar -f http://ftp.gwdg.de/pub/linux/packman/suse/openSUSE_Leap_42.3/ packman
+zypper ar -f http://download.opensuse.org/repositories/Education/openSUSE_Leap_42.3/ education
+zypper ar -f http://download.opensuse.org/repositories/devel:/libraries:/ACE:/major/openSUSE_Leap_42.3/ ace
+
+
+
+# Install requirements for Orchestra and ESE.  Some, like 'rpm' and 'libz' are
+# already in the default image.
+zypper --gpg-auto-import-keys --non-interactive install \
+   make   imake   rsync   socat   xmessage   xclock   emacs \
+   which  xorg-x11-fonts   libXpm4-32bit   libXm4-32bit \
+   libUil4-32bit   libMrm4-32bit glibc-32bit   libgcc_s1-32bit   
+
+# More generic utilities
+zypper --gpg-auto-import-keys --non-interactive install \
+   vim vim-data tcsh sudo tar which less xterm wget \
+   hostname cmake gcc gcc-c++ xeyes postgresql-plpython \
+   python-virtualenv python-psycopg2
+   # texlive-latex texlive-extratools texlive-dvips \
+   # texlive-beamer texlive-collection-fontsextra \
+   # texlive-collection-fontsrecommended
+
+# To buld and install AFNI:
+zypper --gpg-auto-import-keys --non-interactive install \
+   libXft-devel libXp-devel libXpm-devel \
+   libXmu-devel libpng12-devel libjpeg62 \
+   zlib-devel libXt-devel libXext-devel \
+   libXi-devel libexpat-devel netpbm m4 \
+   libnetpbm-devel libGLU1 motif motif-devel \
+   gsl-devel glu-devel freeglut-devel \
+   netcdf netcdf-devel glib2-devel R-base-devel
+
+# Gadgetron and ISMRMRD development:
+zypper --gpg-auto-import-keys --non-interactive install \
+   python-devel python-numpy python-numpy-devel \
+   python-matplotlib python-Sphinx \
+   doxygen dcmtk dcmtk-devel libdcmtk3_6 \
+   armadillo-devel libarmadillo7 glew glew-devel git \
+   ace ace-devel boost-devel fftw3 fftw3-devel hdf5 hdf5-devel
+
+# For Siemens-ISMRMRD converter:
+zypper --gpg-auto-import-keys --non-interactive install \
+   xsd libxerces-c-devel libxslt-devel tinyxml-devel libxml2-devel
+
+
+
+# Make a few links needed for various software suites.
+ln -s  /usr/lib64/libpng16.so.16.8.0  /usr/lib64/libpng.so
+ln -s  /lib64/libz.so.1  /lib64/libz.so
+

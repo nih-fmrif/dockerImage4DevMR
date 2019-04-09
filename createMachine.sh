7130497484b676ebd182aaac4b33a5cb26f22093
V. R <for_usenet@yahoo.com>
2019-04-09 13:50:56 -0400

    Update container build to remove most Python 2 components, and move to Python 3.  Update status of issues with gcc and boost versions.

--- createMachine.sh
+++ createMachine.sh
@@ -22,7 +22,7 @@ zypper --gpg-auto-import-keys --non-interactive install \
    vim vim-data tcsh sudo tar which less xterm wget \
    hostname cmake gcc gcc-c++ xeyes postgresql-plpython \
    gcc6  gcc6-c++  gcc6-fortran \
-   python-virtualenv python-psycopg2
+   python3-virtualenv python3-psycopg2
    # texlive-latex texlive-extratools texlive-dvips \
    # texlive-beamer texlive-collection-fontsextra \
    # texlive-collection-fontsrecommended
@@ -39,31 +39,56 @@ zypper --gpg-auto-import-keys --non-interactive install \
 
 # Gadgetron and ISMRMRD development:
 zypper --gpg-auto-import-keys --non-interactive install \
-   python-devel python-numpy python-numpy-devel \
-   python-matplotlib python-Sphinx \
+   python3-devel python3-numpy python3-numpy-devel \
+   python3-matplotlib python3-Sphinx \
    doxygen dcmtk dcmtk-devel libdcmtk3_6 \
    cblas armadillo-devel glew glew-devel git \
-   ace ace-devel fftw3 fftw3-devel hdf5 hdf5-devel # \
-   # The distributed version of these libraries in openSUSE 42.3
-   # are NOT built with the C++ 14 standard, so when used to build
-   # the Gadgetron, there are linking errors.  For now, an up to
-   # date version of Boost will be downloaded and built with the
-   # appropriate flags, and for Gadgetron compilation, the
-   # variables and values: CMAKE_C_COMPILER=/usr/bin/gcc-6, 
-   # CMAKE_CXX_COMPILER=/usr/bin/g++-6, and
-   # Boost_INCLUDE_DIR=$HOME/my_root/boost/include    will be used
-   # as part of the cmake command to configure the build process.
+   ace ace-devel fftw3 fftw3-devel # hdf5 hdf5-devel \ # SuSE-supplied
+   # HDF version not compatible with Orchestra development environment.
+   # Use libraries supplied with that environment instead.
    #
-   # libboost_date_time1_68_0 libboost_date_time1_68_0-devel \
-   # libboost_filesystem1_68_0 libboost_filesystem1_68_0-devel \
-   # libboost_program_options1_68_0 libboost_program_options1_68_0-devel \
-   # libboost_python-py2_7-1_68_0 libboost_python-py2_7-1_68_0-devel \
-   # libboost_python-py3-1_68_0 libboost_python-py3-1_68_0-devel \
-   # libboost_regex1_68_0 libboost_regex1_68_0-devel \
-   # libboost_system1_68_0 libboost_system1_68_0-devel \
-   # libboost_test1_68_0 libboost_test1_68_0-devel \
-   # libboost_timer1_68_0 libboost_timer1_68_0-devel \
-   # libboost_thread1_68_0 libboost_thread1_68_0-devel
+   #
+   #
+   # The distributed version (1.68) of boost libraries originally
+   # distributed with openSUSE 42.3 were NOT built with the C++ 14
+   # standard, so when used to build the Gadgetron, there were
+   # linking errors.  To work around this, a current version of 
+   # Boost was downloaded and built with the appropriate flags and
+   # for Gadgetron compilation, the variables and values:
+   #
+   #    CMAKE_C_COMPILER=/usr/bin/gcc-6, 
+   #    CMAKE_CXX_COMPILER=/usr/bin/g++-6
+   #    Boost_INCLUDE_DIR=$HOME/my_root/boost/include
+   #
+   # were used as part of the cmake command line.
+   #
+   #
+   #
+   # As of January 2019, building with openSUSE's gcc-6 and g++-6
+   # and version 1.69 of the system's boost libraries will allow
+   # Gadgetron to successfully build.
+
+zypper --gpg-auto-import-keys --non-interactive install \
+   boost-license1_69_0            libboost_headers1_69_0-devel \
+   libboost_chrono1_69_0          libboost_chrono1_69_0-devel \
+   libboost_container1_69_0       libboost_container1_69_0-devel \
+   libboost_date_time1_69_0       libboost_date_time1_69_0-devel \
+   libboost_filesystem1_69_0      libboost_filesystem1_69_0-devel \
+   libboost_program_options1_69_0 libboost_program_options1_69_0-devel \
+   libboost_python3-devel \
+   libboost_python-py3-1_69_0     libboost_python-py3-1_69_0-devel \
+   libboost_regex1_69_0           libboost_regex1_69_0-devel \
+   libboost_system1_69_0          libboost_system1_69_0-devel \
+   libboost_thread1_69_0          libboost_thread1_69_0-devel \
+   libboost_timer1_69_0           libboost_timer1_69_0-devel \
+   libboost_test1_69_0            libboost_test1_69_0-devel
+
+# to try to 'python3-ize' the installation as much as possible
+zypper --non-interactive remove python python-base python-devel
+zypper --gpg-auto-import-keys --non-interactive install \
+   cmake git python3-base python3-devel python3-numpy \
+   python3-numpy-devel python3-Cython python3-six \
+   python3-pip python3-setuptools python3-virtualenv
 
 # Link to latest gcc for Gadgetron development, per documentation at:
 #
@@ -83,4 +108,6 @@ zypper --gpg-auto-import-keys --non-interactive install \
 # Make a few links needed for various software suites.
 ln -s  /usr/lib64/libpng16.so.16.8.0  /usr/lib64/libpng.so
 ln -s  /lib64/libz.so.1  /lib64/libz.so
+# to have libboost link to python3 version
+rm /usr/lib64/libboost_python.so ; ln -s /usr/lib64/libboost_python3.so /usr/lib64/libboost_python.so
 
